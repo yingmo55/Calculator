340f8dc25c6897b7b54157beecf2ef3435684be0
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-04 20:37:22 -0500

    adjust display

--- src/Components/Display/Display.js
+++ src/Components/Display/Display.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import './Display.css';
 
-class Display extends React.Component {
+function Display(props) {
 
-    render(){
-        let display = 0;
-        let operationKey = this.props.operationKey;
-        return(
-            <div className='display'>
-                <p>{operationKey}</p>
-                <p>{display.toLocaleString("en-US")} </p>
-            </div>
-        );
-    }
+    let display = props.display;
+    return(
+        <div className='display'>
+            <p></p>
+            <p>{display} </p>
+        </div>
+    );
 }
 
 export default Display
